8150417d28b811c0da39280dee69afeed84fb753
Catalin Enache <catalinenache78@gmail.com>
2023-06-22 16:25:19 +0300

    Added warning for image sizes

--- atlas.c
+++ atlas.c
@@ -31,7 +31,7 @@ main( int argc, char **argv )
     if (kv_arr == NULL)
         return exit_with_error("Could not extract kv_arr config\n");
 
-    printf("Config lines found: %d\n", num_entries);
+    printf("\nConfig lines found: %d\n", num_entries);
 
     for (int i = 0; i < num_entries; i++) {
         printf(" - ");
@@ -42,7 +42,7 @@ main( int argc, char **argv )
     int cols = config_find(kv_arr, num_entries, "cols")->value.i;
     int compression = config_find(kv_arr, num_entries, "compression")->value.i;
     free(kv_arr);
-    printf("Config values: shrink: %lf, cols %d, compression %d \n", shrink, cols, compression);
+    printf("\nConfig values: shrink: %lf, cols %d, compression %d \n\n", shrink, cols, compression);
 
     int num_paths;
     char **paths = list_files(IMAGES_DIR, &num_paths, true);
@@ -54,9 +54,17 @@ main( int argc, char **argv )
     VipsImage *in_array[num_paths];
     VipsImage *out_array[num_paths];
 
+    int last_image_width, last_image_height, warning_for_different_size_emitted;
     for(int i = 0; i < num_paths; i++) {
-        if( !(in_array[i] = vips_image_new_from_file( paths[i], NULL )) )
-            vips_error_exit( NULL );
+        if(!(in_array[i] = vips_image_new_from_file( paths[i], NULL )))
+            return exit_with_error("Could not create image from file %s.\n", paths[i]);
+        if (i > 0
+        && (in_array[i]->Xsize != last_image_width || in_array[i]->Ysize != last_image_height)
+        && !warning_for_different_size_emitted) {
+            warning_for_different_size_emitted = 1;
+        }
+        last_image_width = in_array[i]->Xsize;
+        last_image_height = in_array[i]->Ysize;
         int h_shrinked, v_shrinked;
         zoom_out(in_array[i], &(out_array[i]), shrink, shrink, &h_shrinked, &v_shrinked);
         printf("Found image: %s [%d * %d] => [%d * %d]\n", paths[i], in_array[i]->Xsize, in_array[i]->Ysize, h_shrinked, v_shrinked);
@@ -64,6 +72,9 @@ main( int argc, char **argv )
         g_object_unref(in_array[i]);
     }
     free(paths);
+    if (warning_for_different_size_emitted)
+        printf("\nWarning: At least one image does not match the size of other images.\n");
+
 
     VipsImage *atlas = NULL;
     if (vips_arrayjoin(out_array, &atlas,  num_paths, "across", cols, NULL))
